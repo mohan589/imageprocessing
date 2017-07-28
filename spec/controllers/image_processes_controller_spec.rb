b45ca6733bb4f4780b616897511056c25d41bc57
mohan <mohan589@gmail.com>
2017-07-28 20:23:11 +0530

    changes for spec

--- spec/controllers/image_processes_controller_spec.rb
+++ spec/controllers/image_processes_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ImageProcessesController, type: :controller do
+  describe "Create #create" do
+    context 'with valid attributes,responds successfully with an HTTP 200 status code' do
+      image = FactoryGirl.create(:user, :username => "jdoe", :password => "secret")
+    end
 
+    context 'with invalid attributes' do
+      image = FactoryGirl.create(:user, :username => "jdoe", :password => "secret")
+    end
+  end
 end
