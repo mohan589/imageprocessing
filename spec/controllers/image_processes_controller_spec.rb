c2e37ec5c297ad0a80f28c0ad1e28307a8b3b35d
mohan <mohan589@gmail.com>
2017-07-28 21:50:48 +0530

    changes for views

--- spec/controllers/image_processes_controller_spec.rb
+++ spec/controllers/image_processes_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ImageProcessesController, type: :controller do
     context 'with valid attributes,responds successfully with an HTTP 200 status code' do
       it "creates a new image and will be placed in public folder" do
         post 'create', image_process: FactoryGirl.attributes_for(:image_process)
-        expect(response).to redirect_to(:show)
+        expect(response).to render_template(:create)
       end
     end
 
